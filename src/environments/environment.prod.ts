526ff1aa5f4977de6f3d2f8762e704776960ccbe
arivuselvna@gmail.com <arivuselvan@gmail.com>
2020-04-02 00:36:08 +0530

    Initial changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAyrf9UWMd_VCqadGhcfHPyNZqZJEycRoY",
+    authDomain: "mystore-angular.firebaseapp.com",
+    databaseURL: "https://mystore-angular.firebaseio.com",
+    projectId: "mystore-angular",
+    storageBucket: "mystore-angular.appspot.com",
+    messagingSenderId: "209195841761",
+    appId: "1:209195841761:web:53f1bf996ddbd962c91fa0",
+    measurementId: "G-SKYTNW49GC"
+  }
 };
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
